8cbbec74b24f7f93ff7b3c809f9cb53ecfc6ac91
ислам <islam@MacBook-Air-islam.local>
2026-04-05 03:10:25 +0300

    Rename license API integration test to run in surefire

--- src/test/java/com/recruting/demo/license/LicenseTicketSignatureApiTest.java
+++ src/test/java/com/recruting/demo/license/LicenseTicketSignatureApiTest.java
@@ -34,7 +34,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
-class LicenseTicketSignatureApiIT {
+class LicenseTicketSignatureApiTest {
 
     @Autowired
     private MockMvc mockMvc;
